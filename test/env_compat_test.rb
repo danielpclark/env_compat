dd508639ddb031b1628181116e94f23a9af095fd
Daniel P. Clark <6ftdan@gmail.com>
2016-02-26 20:02:51 -0500

    FYI: Must have underscore mapping

--- test/env_compat_test.rb
+++ test/env_compat_test.rb
@@ -10,4 +10,8 @@ describe EnvCompat do
     v = EnvCompat.decode "HTTPS_CFF_GITHUB_D_COM_F_COMPANY_F_PRIVATE_M_REPO_D_GIT" 
     _(v).must_equal "https://github.com/company/private-repo.git".upcase
   end
+
+  it "must have an underscore character mapping" do
+    _(EnvCompat.instance_eval {mapping.values}).must_include '_'
+  end
 end
